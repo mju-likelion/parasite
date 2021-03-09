cd7a6f4bf8b5eab5cd63f8a4d8d86960882bd376
ndaemy <yuyaebean@gmail.com>
2021-03-10 04:22:47 +0900

    Feat: Improve login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,74 @@
+import styled from "@emotion/styled";
 import axios from "axios";
 import { useFormik } from "formik";
 import { useRouter } from "next/router";
+
+const Self = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const LoginBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 6px;
+  padding: 24px 0;
+  background-color: #28292a;
+  width: 375px;
+
+  @media (max-width: 768px) {
+    flex: 1;
+    width: auto;
+    height: 100%;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 22px;
+  letter-spacing: -0.1px;
+  margin: 24px 0 16px;
+`;
+
+const Input = styled.input`
+  width: 335px;
+  height: 44px;
+  background: #3a3b3d;
+  border-radius: 6px;
+  border: none;
+  margin: 8px 0 0;
+  padding: 11px 12px;
+  font-size: 17px;
+  font-weight: 22px;
+`;
+
+const LogInButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ff921b;
+  width: 335px;
+  height: 44px;
+  border-radius: 6px;
+  margin: 20px 0 25px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 22px;
+  letter-spacing: -0.2px;
+`;
 
 function Home() {
   const router = useRouter();
@@ -30,21 +98,29 @@ function Home() {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <input
-        id="email"
-        name="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <input
-        id="password"
-        name="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <Self>
+      <LoginBlock>
+        <Form onSubmit={formik.handleSubmit}>
+          <Title>로그인</Title>
+          <Input
+            id="email"
+            name="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <Input
+            id="password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <LogInButton type="submit">Login</LogInButton>
+        </Form>
+      </LoginBlock>
+    </Self>
   );
 }
 
